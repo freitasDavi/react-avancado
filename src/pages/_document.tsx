49cca8b67ff88f15e2548bd0df431939615d5534
Davi Freitas <davi.frrs@gmail.com>
2020-08-19 10:04:23 -0300

    Added global styles and styledComponents

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,10 @@
-import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
@@ -7,24 +13,24 @@ export default class MyDocument extends Document {
     const originalRenderPage = ctx.renderPage
 
     try {
-        ctx.renderPage = () =>
-            originalRenderPage({
-                enhanceApp: (App) => (props) =>
-                    sheet.collectStyles(<App {...props} />)
-            })
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        })
 
-        const initialProps = await Document.getInitialProps(ctx)
-        return {
-            ...initialProps,
-            styles: (
-                <>
-                 {initialProps.styles}
-                 {sheet.getStyleElement()}
-                </>
-            )
-        }
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      }
     } finally {
-        sheet.seal()
+      sheet.seal()
     }
   }
 
